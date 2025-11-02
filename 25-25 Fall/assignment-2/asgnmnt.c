d12f78f090bdd1e4181080f2450afb1e04fa99e1
kataw1na <memetali308@gmail.com>
2025-11-02 23:54:33 +0300

    update assgnmnt.c
    Hocam arkadasim beni uyardi, once odevi pushlamam gerektigi sonrasinda kodu yazmamin daha iyi oldugunu soyledi.

--- 25-25 Fall/assignment-2/asgnmnt.c
+++ 25-25 Fall/assignment-2/asgnmnt.c
@@ -1,12 +1,137 @@
-// Ad:
-// Soyad:
-// Okul No:
-// Sınıf:
-// Öğrenim Türü (Örgün/İkinci Öğretim):
-
+// Ad: Mehmet Ali
+// Soyad: Dönmez
+// Okul No: 232503026
+// Sınıf: 3. Sınıf 
+// Öğrenim Türü (Örgün/İkinci Öğretim): Örgün Öğretim
+#include <time.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <windows.h>
+// Hizli Siralama (Quick Sorting)
+// dusuk = dizinin baslangic indexi, yuksek = dizinin dizinin bitis indexi
+// index1, index2 kendi arasinda degistirilecek (takaslanacak) degerler
+
+// Quick Sort
+void quickSort(int dizi[], int dusuk, int yuksek);
+void eleman_swap(int *index1, int *index2);
+int dizi_bolme(int dizi[], int dusuk, int yuksek);
+
+
+// Merge Sort
+void mergeSort(int dizi[], int sol, int sag);
+void birlesim(int dizi[], int sol, int orta, int sag);
+
+
+
+
 
 int main(){
-    printf("Merak etme, ödevler gayet basit.\nKendine güven ve bunu tamamla!\n");
+   // printf("Merak etme, ödevler gayet basit.\nKendine güven ve bunu tamamla!\n");
+
+    int dizi[1000];
+
+    srand(time(NULL)); // ayni sayilarin gelmesini onler
+    // Diziye 1 - 10.000 arasi random sayilar ata.
+    for(int a = 0; a < 1000; a++){
+        dizi[a] = rand() % 10000 + 1;
+    }
+    // Diziyi ekrana yazdir
+    printf("1000 Elemanli dizi: \n\n");
+    for(int a=0; a < 1000; a++){
+        printf("%d, ", dizi[a]);
+    }
+    
+    /*
+    Hocam sizin ipucu olarak verdiginiz clock() fonksiyonunu kullandigimda islem cok hizli gerceklestigi gerekcesiyle surekli
+    calisma suresini 0 saniye olarak veriyordu. Bende biraz arastirip bu yontemi buldum. Bu sure hesaplama yontemiyle yaklasik
+    bir sonuc aldim. Odevde istediginiz karsilastirmayi yapabilmek icin sure hesaplama yontemini bu sekilde yaziyorum.
+        
+    Umarim kabul edersiniz 
+    */
+    LARGE_INTEGER start, end, freq;
+    QueryPerformanceFrequency(&freq);
+    QueryPerformanceCounter(&start);
+
+    quickSort(dizi, 0, 999);
+
+    QueryPerformanceCounter(&end);
+
+    double qS_time = (double)(end.QuadPart - start.QuadPart) / freq.QuadPart;
+ 
+    printf("\n\nHizli Siralama algoritmasi ile siralanmis dizi: \n\n");
+    for(int a = 0; a < 1000; a++){
+        printf("%d, ", dizi[a]);
+    }
+    printf("\n\nAlgoritma calisma hizi: %.8f saniye", qS_time);
+
     return 0;
 }
+
+// --------------------------------------- HIZLI SIRALAMA ALGORITMASI ---------------------------------------------
+// Algoritma fonksiuyonu
+void quickSort(int dizi[], int dusuk, int yuksek){
+ 
+    if(dusuk < yuksek){
+        int pivot = dizi_bolme(dizi, dusuk, yuksek);
+
+        // Pivot degerinin sol tarafindaki kisim
+        quickSort(dizi, dusuk, pivot -1);
+        // Pivot degerinin sag tarafindaki kisim
+        quickSort(dizi, pivot + 1, yuksek);
+
+    }
+}
+
+// Dizi Eleman Takas fonksiyonu
+void eleman_swap(int *index1, int *index2){
+
+    // tmp (Gecici) integer sayesinde index1 ve index2 degerlerini kendi aralarinda takasliyorum
+
+    int tmp;
+    tmp = *index1;
+    *index1 = *index2;
+    *index2 = tmp;
+}
+
+// Diziyi sectigimiz pivot elemana gore bolen fonksiyon
+int dizi_bolme(int dizi[], int dusuk, int yuksek){
+
+    int pvt = dizi[yuksek]; // Dizinin son elemanini pivot olarak seciyorum.
+
+    // dusuk_index'e -1 degeri vererek henuz bir dizinin elemanini gostermemesini sagliyorum. Takas isleminden once 1 arttiriyorum.
+    int dusuk_index = dusuk - 1;
+
+    for(int i = dusuk; i < yuksek; i++){
+        if(dizi[i] < pvt){
+            dusuk_index++;
+            eleman_swap(&dizi[dusuk_index], &dizi[i]); // Eleman pivottan kucuk ise pivotun soluna tasinir.
+        }
+    }
+
+    // Pivot degerini dogru indexe yerlestirir
+    eleman_swap(&dizi[dusuk_index + 1], &dizi[yuksek]);
+    // Pivot'un yeni index degerini dondurur
+    return dusuk_index + 1;
+}
+// --------------------------------------- HIZLI SIRALAMA ALGORITMASI ---------------------------------------------
+
+// --------------------------------------- BIRLESTIRME SIRALAMA ALGORIMASI ---------------------------------------------
+void mergeSort(int dizi[], int sol, int sag){
+    if( sol < sag){
+        int orta = sol + ((sag - sol) / 2); // Ortayi hesaplama
+        mergeSort(dizi, sol, orta);         // Sol yarim diziyi siralar
+        mergeSort(dizi, orta + 1, sag);     // Sag yarim diziyi siralar
+        merge(dizi, sol, orta, sag);        // Dizileri birlestirir
+    }
+}
+
+void birlesim(int dizi[], int sol, int orta, int sag){
+    int sol_boyut = orta - (sol + 1);  // Sol yarim dizinin boyutu
+    int sag_boyut = sag - orta;        // Sag yarim dizinin boyutu
+
+    int sol_dizi[sol_boyut];
+    int sag_dizi[sag_boyut];
+
+    for(int i=0; i < sol_boyut;);
+}
+// --------------------------------------- BIRLESTIRME SIRALAMA ALGORIMASI ---------------------------------------------
